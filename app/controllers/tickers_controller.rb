126a28063e4e773d4f69c9351568ef5b506b916e
Chris Siegman <christopher.siegman@gmail.com>
2020-05-18 14:43:44 -0500

    Add show, create, and params helper methods to Ticker controller

--- app/controllers/tickers_controller.rb
+++ app/controllers/tickers_controller.rb
@@ -3,8 +3,27 @@ class TickersController < ApplicationController
   def index
   end
 
+  def show
+    @ticker = Ticker.find(params[:id])
+  end
+
+  def create
+    @ticker = Ticker.new(ticker_params)
+
+    if @ticker.save
+      redirect_to @ticker
+    else
+      render 'new'
+    end
+  end
+
   def new
     @ticker = Ticker.new
+  end
+
+  private
+  def ticker_params
+    params.require(:ticker).permit(:name)
   end
 
 end
