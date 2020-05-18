c3b65550c1675c2bb2d7546cc821b460c5468d44
Chris Siegman <christopher.siegman@gmail.com>
2020-05-18 11:15:19 -0500

    Add resource for tickers

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  resources :tickers
+
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
