4de7185c0fb439796770d47448b3d4e90a070000
Chris Siegman <christopher.siegman@gmail.com>
2020-05-19 12:11:19 -0500

    Set sessions#welcome as root url

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
     resources :prices
   end
 
-  root 'welcome#index'
+  #root 'welcome#index'
+  root 'sessions#welcome'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
