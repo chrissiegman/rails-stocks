0d0cdae96ae5f3456f589e53e0ca9215b48c9fd3
Chris Siegman <christopher.siegman@gmail.com>
2020-05-19 12:42:28 -0500

    Add helper methods to ApplicationController and create method to User Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,14 @@ class UsersController < ApplicationController
   end
 
   def create
+
+    # Need to add validation
+    @user = User.create(params.require(:user).permit(:username, :password))
+
+    session[:user_id] = @user.id
+
+    redirect_to '/welcome'
+
   end
+
 end
