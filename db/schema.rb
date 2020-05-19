90fb085d6cb708980495e0f8ccb309e1c94dfa61
Chris Siegman <christopher.siegman@gmail.com>
2020-05-19 12:10:47 -0500

    DB schema update for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_135651) do
+ActiveRecord::Schema.define(version: 2020_05_19_165204) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -39,6 +39,13 @@ ActiveRecord::Schema.define(version: 2020_05_19_135651) do
 
   create_table "tickers", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
