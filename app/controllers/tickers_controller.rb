f66aa2012292a53e05847a063df403cb7aafb0aa
Chris Siegman <christopher.siegman@gmail.com>
2020-05-20 14:29:16 -0500

    Add ticker validation to preserve data integrity and minimize API usage

--- app/controllers/tickers_controller.rb
+++ app/controllers/tickers_controller.rb
@@ -9,15 +9,19 @@ class TickersController < ApplicationController
   end
 
   def create
-    ticker = Ticker.find_by name: params[:ticker][:name]
-    if ticker 
-      flash[:notice] = "The ticker " + ticker[:name] +  " is already in your watchlist"
+    # I think this logic could be more concise
+    params[:ticker][:name] = params[:ticker][:name].upcase
+    ticker = Ticker.find_by name: params[:ticker][:name] 
+    if ticker
+      flash[:error] = "The ticker " + ticker[:name] +  " is already in your watchlist."
     else
       @ticker = Ticker.new(ticker_params)
-      if @ticker.save
-        insert_ticker_price_data(@ticker.name)
+      data = call_chart_api(@ticker.name)
+      if !data.nil?
+        @ticker.save
+        insert_ticker_price_data(@ticker.name, data)
       else
-        flash[:notice] = "Something went wrong."
+        flash[:error] = "Data import failed for symbol " + @ticker.name + ". Please ensure this is a valid symbol and try again."
       end
     end
     redirect_to tickers_path
@@ -34,11 +38,8 @@ class TickersController < ApplicationController
   end
 
   private
-  def insert_ticker_price_data(symbol)
-    # make this dependent on success
-    flash[:notice] = "Added ticker " + @ticker.name + " to your watchlist."
-    
-    data = call_chart_api(symbol)
+  def insert_ticker_price_data(symbol, data)
+    flash[:success] = "Added ticker " + @ticker.name + " to your watchlist."
     data.each do |key, value|
       ticker = Ticker.find_by name: symbol
       ticker.prices.create({"timestamp": key, "value": value})
@@ -47,20 +48,23 @@ class TickersController < ApplicationController
 
   private
   def call_chart_api(symbol)
-    #TODO handle bad response from API
-
     # Build API url string
     response = Unirest.get "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-charts?region=US&lang=en&interval=1d&range=1y&symbol=" + symbol,
       headers:{
         "X-RapidAPI-Host" => "apidojo-yahoo-finance-v1.p.rapidapi.com",
         "X-RapidAPI-Key" => "3f652748f1mshbfd8df51ee5fa08p1bfff1jsn074838544e52",
       }
-
     content = response.raw_body
-    result = JSON.parse(content)["chart"]["result"][0]
-    timestamps = result["timestamp"]
-    prices = result["indicators"]["quote"][0]["close"]
-    data = Hash[timestamps.zip(prices)]
+    result = JSON.parse(content)["chart"]["result"]
+
+    if !result.nil?
+      result = JSON.parse(content)["chart"]["result"][0]
+      timestamps = result["timestamp"]
+      prices = result["indicators"]["quote"][0]["close"]
+      data = Hash[timestamps.zip(prices)]
+    else
+      data = nil
+    end
   end
 
 end
