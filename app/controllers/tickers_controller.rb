21de7c447c99cee519fd1ce2bc16b6c25b9a7c99
Chris Siegman <christopher.siegman@gmail.com>
2020-05-19 21:14:17 -0500

    More styling

--- app/controllers/tickers_controller.rb
+++ app/controllers/tickers_controller.rb
@@ -1,5 +1,7 @@
 class TickersController < ApplicationController
 
+  skip_before_action :authorized, only: [:index]
+
   def index
     @tickers = Ticker.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
   get 'authorized', to: 'sessions#page_requires_login'
+  get 'signup', to: 'users#new'
 
   resources :users, only: [:new, :create]
 
