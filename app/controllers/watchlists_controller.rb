554882cea6336cf99a67545e489bc32e272de538
Chris Siegman <christopher.siegman@gmail.com>
2020-05-20 18:44:24 -0500

    Correctly select watchlists for users

--- app/controllers/watchlists_controller.rb
+++ app/controllers/watchlists_controller.rb
@@ -4,10 +4,22 @@ class WatchlistsController < ApplicationController
     @watchlists = current_user.watchlists
   end
 
+  def show
+    @watchlist = Watchlist.find_by(params[:id])
+  end
+
   def new
     @watchlist = Watchlist.new
   end
 
   def create
+    @watchlist = Watchlist.create(:user_id => session[:user_id], :name => watchlist_params[:name])
+    redirect_to watchlist_path(@watchlist.id)
   end
+
+  private
+  def watchlist_params
+    params.require(:watchlist).permit(:name, :user_id)
+  end
+
 end
